8ecfefa7c44703880dc0ce6deb8dfc3a2ee03298
DrewH95 <andrew.j.hughes333@gmail.com>
2022-01-16 22:26:38 -0600

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PersomCardComponent from './componets/PersonCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <PersomCardComponent firstName = "Ted" lastName = "Mosby" age = {28} hairColor = "Black"/>
+      <PersomCardComponent firstName = "John" lastName = "Smith" age = {900} hairColor = "Brown" />
+      <PersomCardComponent firstName = "Ted" lastName = "Lasso" age = {36} hairColor = "Brown"/>
+      <PersomCardComponent firstName = "Lumpy" lastName = "Space-Princess" age = {22} hairColor = "Purple"/>
     </div>
   );
 }
